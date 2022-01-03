1c0f5f36fed0c1ae2b5e67b0f07243e5b1850df2
VuDej <93735137+VuDej@users.noreply.github.com>
2022-01-03 13:30:03 +0100

    Add new book

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
 /* MOCH DATABASE    */
 const books = [
-  {
-    name: 'Book1',
-    author: 'Author1',
-  },
-  {
-    name: 'Book2',
-    author: 'Author2',
-  },
+  // {
+  //   name: 'Book1',
+  //   author: 'Author1',
+  // },
+  // {
+  //   name: 'Book2',
+  //   author: 'Author2',
+  // },
 ];
+console.log(books);
 
 /*  RENDERING BOOKS  */
 const renderBooks = document.getElementById('render-books');
 let renderItem = '';
-
+function render() {
 for (let i = 0; i < books.length; i += 1) {
   renderItem = `
   <ul>
-    <li>${books[i].name}</li>
+    <li>${books[i].title}</li>
     <li>${books[i].author}</li>
     <li><a id='delete' href="#">Remove</a></li>
   </ul>
+  <hr>
   `;
 
   renderBooks.innerHTML += renderItem;
 }
 
-console.log(renderBooks);
+}
+
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const addBook = document.querySelector('#addBook');
+
+
+addBook.addEventListener('click', (e)  => {
+     let title = titleInput.value;
+     let author = authorInput.value;
+     let newBook = {title, author};
+     books.push(newBook)
+     console.log('books:', books)
+
+render();
+
+
+});
