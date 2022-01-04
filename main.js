6a60b22aa8d1c747cb5e765972ab2eb18f63fb6d
Nuri1977 <nurilacka@gmail.com>
2022-01-04 14:35:50 +0100

    Add class methods to add and remove books

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
 /* VARIABLES   */
 let books = [];
-const renderBooks = document.getElementById('render-books');
+const renderBooks = document.getElementById('render-content');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const addBook = document.querySelector('#addBook');
 const localBooks = localStorage.getItem('booksStore');
 
+/* CLASS BOOK */
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  static addItem(title, author) {
+    if (title !== '' && author !== '') {
+      const newBook = new Book(title, author);
+      books.push(newBook);
+    } else {
+      alert('Please fill out the fields in the book form');
+    }
+  }
+
+  static deleteItem(deleteName) {
+    books = books.filter((book) => book.title !== deleteName);
+  }
+}
+
 /* Functions */
-// function render() {
-//   renderBooks.innerHTML = '';
-//   for (let i = 0; i < books.length; i += 1) {
-//     let renderItem = '';
-//     renderItem = `
-//     <ul>
-//       <li>${books[i].title}</li>
-//       <li>${books[i].author}</li>
-//       <li><a id="delete" name="${books[i].title}" href="#">Remove</a></li>
-//     </ul>
-//     <hr>
-//     `;
-//     renderBooks.innerHTML += renderItem;
-//   }
-// }
+function render() {
+  renderBooks.innerHTML = '';
+  for (let i = 0; i < books.length; i += 1) {
+    let renderItem = '';
+    renderItem = `
+    <tr>
+      <td>
+        <h2>"${books[i].title}" by ${books[i].author}</h2>
+      </td>
+      <td><a class='delete' name="${books[i].title}" href="#">Remove</a></td>
+    </tr>
+    `;
+    renderBooks.innerHTML += renderItem;
+  }
+}
 
 function getStore() {
   if (localBooks) {
@@ -33,12 +54,9 @@ function setStore() {
   localStorage.setItem('booksStore', JSON.stringify(books));
 }
 
-function addNewBook() {
-  const newBook = {
-    title: titleInput.value,
-    author: authorInput.value,
-  };
-  books.push(newBook);
+function addNewBook(event) {
+  event.preventDefault();
+  Book.addItem(titleInput.value, authorInput.value);
   setStore();
   titleInput.value = '';
   authorInput.value = '';
@@ -46,8 +64,7 @@ function addNewBook() {
 }
 
 function deleteBook(e) {
-  const deleteName = e.target.name;
-  books = books.filter((book) => book.title !== deleteName);
+  Book.deleteItem(e.target.name);
   setStore();
   render();
 }
